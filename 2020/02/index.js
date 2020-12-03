9e90d69272a4f228126cf5a096fcc4ba9bcada7c
G100g <giorgio@g100g.net>
2020-12-03 22:01:53 +0100

    chore: add 02 part two solution

--- 2020/02/index.js
+++ 2020/02/index.js
@@ -3,31 +3,41 @@ const { readFile } = require("../utils");
 const main = async () => {
   const file = await readFile(__dirname + "/input.txt", "utf8");
 
-  let solution = 0;
+  let solutionPartOne = 0;
+  let solutionPartTwo = 0;
 
-  const passwords = file
-    .split("\n")
-    .map((row) => {
-      const r = row.split(": ");
-      const char = r[0].split(" ")[1];
-      const range = r[0].split(" ")[0].split("-").map(Number);
+  const passwords = file.split("\n").map((row) => {
+    const r = row.split(": ");
+    const char = r[0].split(" ")[1];
+    const range = r[0].split(" ")[0].split("-").map(Number);
 
-      return {
-        char,
-        range,
-        password: r[1],
-      };
-    })
-    .forEach(({ password, range, char }) => {
-      const charCount = password.replace(new RegExp(`[^${char}]`, "gi"), "")
-        .length;
+    return {
+      char,
+      range,
+      password: r[1],
+    };
+  });
 
-      if (charCount >= range[0] && charCount <= range[1]) {
-        solution++;
-      }
-    });
+  passwords.forEach(({ password, range, char }) => {
+    const charCount = password.replace(new RegExp(`[^${char}]`, "gi"), "")
+      .length;
 
-  console.log(solution);
+    if (charCount >= range[0] && charCount <= range[1]) {
+      solutionPartOne++;
+    }
+  });
+
+  console.log(solutionPartOne);
+
+  passwords.forEach(({ password, range, char }) => {
+    if (
+      (password[range[0] - 1] == char && password[range[1] - 1] != char) ||
+      (password[range[0] - 1] != char && password[range[1] - 1] == char)
+    ) {
+      solutionPartTwo++;
+    }
+  });
+  console.log(solutionPartTwo);
 };
 
 main();
