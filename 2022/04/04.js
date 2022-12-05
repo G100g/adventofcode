429b0dc3ef8ed9aae90ea1de6cca7ba2aa8a5d01
G100g <gigetto@gmail.com>
2022-12-05 13:50:41 +0100

    feat: day4 2022 - first

---
+++ 2022/04/04.js
@@ -0,0 +1,25 @@
+import { dataToArray, printSolution } from "../utils.js";
+
+const getSectionsFromRange = (range) => {
+    const [from, to] = range.split("-");
+    return [parseInt(from, 10), parseInt(to, 10)];
+};
+
+export default (data) => {
+    const sections = dataToArray(data);
+
+    const overlaps = sections
+        .map((p) => {
+            const [a, b] = p.split(",");
+            const [fromA, toA] = getSectionsFromRange(a);
+            const [fromB, toB] = getSectionsFromRange(b);
+
+            const isAContainedInB = fromA >= fromB && toA <= toB;
+            const isBContainedInA = fromB >= fromA && toB <= toA;
+
+            return isAContainedInB || isBContainedInA;
+        })
+        .filter(Boolean);
+
+    printSolution(overlaps.length);
+};
